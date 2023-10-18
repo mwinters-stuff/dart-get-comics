07b9efc4f1914dfa1fd8d30046ddbbdfbad1490c
Mathew Winters <mathew@winters.org.nz>
2023-10-18 03:58:41 +0000

    new dev container compile on 3.1.3

--- bin/get_comics.dart
+++ bin/get_comics.dart
@@ -1,6 +1,6 @@
 import 'dart:io' as io;
 
-import 'package:dcli/dcli.dart';
+import 'package:args/args.dart';
 import 'package:get_comics/fetch_comic.dart';
 import 'package:get_comics/get_comics.dart';
 import 'package:yaml/yaml.dart';
